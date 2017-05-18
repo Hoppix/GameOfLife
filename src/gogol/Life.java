717ae9c00dae443b6d710749e8639602b80a2836
Hoppix <k.hopfmann@hotmail.de>
2017-05-18 13:18:24 +0200

    added sources

---
+++ src/gogol/Life.java
@@ -0,0 +1,12 @@
+
+package gogol;
+/**
+ * Created by hoppix on 18.05.17.
+ */
+public class Life
+{
+    public static void main(String[] args)
+    {
+        System.out.println("testing");
+    }
+}
