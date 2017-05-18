36c933d167c3ff84a8587f78b8a080bf0710ddb6
Hoppix <k.hopfmann@hotmail.de>
2017-05-18 15:20:11 +0200

    raw gui impl

--- src/gogol/Life.java
+++ src/gogol/Life.java
@@ -8,5 +8,7 @@ public class Life
     public static void main(String[] args)
     {
         System.out.println("testing");
+        GameGrid grid = new GameGrid();
+        new LifeGUI(grid);
     }
 }
