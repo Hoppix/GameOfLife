0d6d8ff98d409817ef4aa4e9cbc29581a8e148b8
Rathalos93 <jonas@sandersoft.de>
2017-06-15 21:14:17 +0200

    impl SpeciesTest, SpeciesLibraryTest

---
+++ src/testing/SpeciesLibraryTest.java
@@ -0,0 +1,29 @@
+package testing;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import gogol.Species;
+import gogol.SpeciesLibrary;
+
+public class SpeciesLibraryTest 
+{
+	SpeciesLibrary testLib = new SpeciesLibrary();
+	
+	@Test
+	public void testGetSpecies()
+	{
+		boolean[][] gliderPattern = new boolean[3][3];
+		gliderPattern[0][1] = true;
+		gliderPattern[1][2] = true;
+		gliderPattern[2][0] = true;
+		gliderPattern[2][1] = true;
+		gliderPattern[2][2] = true;		
+		
+		Species glider = testLib.getSpecies("Glider");
+		assertEquals("Glider", glider.getName());
+		assertEquals("moving", glider.getDescription());
+		assertEquals(3, glider.getSizeX());
+		assertEquals(3, glider.getSizeY());
+		assertArrayEquals(gliderPattern, glider.getPattern());
+	}
+}
