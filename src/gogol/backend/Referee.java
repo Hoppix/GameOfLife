9181936782a1c3ecfe71bab2d25f96dccb706ddd
Hoppix <k.hopfmann@hotmail.de>
2017-06-29 15:21:23 +0200

    field values for referee

--- src/gogol/backend/Referee.java
+++ src/gogol/backend/Referee.java
@@ -1,5 +1,7 @@
 package gogol.backend;
 
+import gogol.cells.Cell;
+
 import java.awt.*;
 
 /**
@@ -9,9 +11,30 @@ public class Referee
 {
 	private final Controller controller;
 
+
+	private static final int PLAYER_AREA_PADDING = 4;
+	private static final int STARTING_CELLS = 0;
+	private static final int CELLS_PER_CYCLE = 0;
+	private static final int CYCLE_LENGTH = 0;
+
+
+	private Rectangle playerRedArea;
+	private Rectangle playerBlueArea;
+
+	private int playerRedCellCount;
+	private int playerBlueScoreCellCount;
+
+	/**
+	 * NYI
+	 */
+	private int playerRedKillCount;
+	private int playerBlueKillCount;
+
 	public Referee(Controller parent)
 	{
 		controller = parent;
+		playerRedArea = new Rectangle(PLAYER_AREA_PADDING,0, (controller.survivalMatrix[0].length) - PLAYER_AREA_PADDING, controller.survivalMatrix.length);
+		playerBlueArea = new Rectangle(controller.survivalMatrix[0].length - PLAYER_AREA_PADDING, 0, (((controller.survivalMatrix[0].length)/2)+ PLAYER_AREA_PADDING), controller.survivalMatrix.length);
 	}
 
 	public Color checkColorArea(int posX, int posY)
